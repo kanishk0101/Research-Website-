0d4b9d12c20507259761d5c46d16c28f520f92bc
Kanishk Agarwal <kanishkagarwal36@gmail.com>
2026-08-28 09:12:33 +0530

    fix: update sarika role to 2nd year student - UM-DAE CEBS

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -150,7 +150,7 @@ export default function MembersPage() {
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             <MemberCard
               name="Sarika"
-              role="Undergraduate Student"
+              role="2nd year Student - UM-DAE CEBS"
             />
           </div>
         </section>
